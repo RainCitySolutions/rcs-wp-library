737c83edf6ce923bc150274d96f9003afd7a5fb7
Blair Cooper <dev@raincitysolutions.com>
2026-05-24 11:15:21 -0700

    Fix PHPStan issue

--- src/RCS/Logging/Timer.php
+++ src/RCS/Logging/Timer.php
@@ -98,7 +98,9 @@ class Timer implements TimerInterface
     public function lap($key = ''): void
     {
         $key = ($key === '') ? 'Lap' : $key;
-        if (isset($this->start)) {
+
+        // Don't reset lap if we haven't started
+        if (0.0 !== $this->start) {
             $this->stop();
             $this->lapTotalTime += ($this->stop - $this->start);
             $this->laps[$key . ' ' . $this->count] = $this->getLapTime();
